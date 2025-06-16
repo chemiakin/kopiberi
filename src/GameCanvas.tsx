88db0dd0f57d38efc5c4ae827248246b5c95ad6c
chemiakin <chemiakind@gmail.com>
2025-06-16 08:56:16 +0700

    coomar fix bar

--- src/GameCanvas.tsx
+++ src/GameCanvas.tsx
@@ -509,7 +509,7 @@ const GameCanvas: React.FC = () => {
       img: '/assets/kebab.png',
       desc: 'Сочный и ароматный. Даёт 20 очков и 2 секунды',
       chance: 0.20,
-      points: 10000,
+      points: 20,
       time: 2,
       floatText: '+20'
     },
